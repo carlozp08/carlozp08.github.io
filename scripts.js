f210c3462b8bbbeb8f48445ea7700fedeffe89b8
Carlos Perez <113104198+carlozp08@users.noreply.github.com>
2023-12-27 17:04:17 -0600

    1.3

--- scripts.js
+++ scripts.js
@@ -1,19 +1,15 @@
-// Initialize Swiper 
-var swiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-  });
-<script src="scripts.js"></script>
-  
-// JavaScript content for future interactivity
+// This file can include any JavaScript you need for your site, 
+// such as interactive functionality or animations.
+
+// Example of a simple script to add a class to the navigation item 
+// based on the current page (this would need additional HTML changes to work)
+document.addEventListener('DOMContentLoaded', (event) => {
+    const navItems = document.querySelectorAll('nav ul li a');
+    const currentPage = window.location.pathname.split("/").pop();
+    
+    navItems.forEach((navItem) => {
+        if (navItem.getAttribute('href') === currentPage) {
+            navItem.classList.add('active');
+        }
+    });
+});
