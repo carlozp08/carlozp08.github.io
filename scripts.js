f5d1c1b622f8c7b71037d034ede1406b7c456d90
Carlos Perez <113104198+carlozp08@users.noreply.github.com>
2023-12-27 17:02:06 -0600

    1.2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
-
+// Initialize Swiper 
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+<script src="scripts.js"></script>
+  
 // JavaScript content for future interactivity
